7ac492da694efa294becdfe2e7022ba19f18e56b
Yoshua_Pequeño <josh27p=git config --global user.email josh27p@gmail.com>
2020-12-12 16:26:56 -0500

    buenas practicas menu

--- menu.js
+++ menu.js
@@ -1,7 +1,19 @@
+const changes = window.matchMedia('screen and (max-width:932px)')
+ 
  const menu = document.querySelector('.menu');
  const button_hamburguer = document.querySelector('.hamburguer_menu')
 
- button_hamburguer.addEventListener('click', showHidden);
+ changes.addListener(validation);
+ function validation(event){
+     if(event.matches){
+         button_hamburguer.addEventListener('click', showHidden);
+     }
+     else{
+         button_hamburguer.removeEventListener('click', showHidden);
+     }
+ }
+ validation(changes)
+
  function showHidden() {
      if(menu.classList.contains('menu_active')){
         menu.classList.remove('menu_active')   
