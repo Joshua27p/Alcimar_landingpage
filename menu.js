32d81f6da47e086cd179e96a7e0cc82ac48bfde0
Paul Tang <paul.tang95@gmail.com>
2020-12-12 19:56:12 -0500

    bug de menu soluciando

--- menu.js
+++ menu.js
@@ -2,23 +2,31 @@ const changes = window.matchMedia('screen and (max-width:932px)')
  
  const menu = document.querySelector('.menu');
  const button_hamburguer = document.querySelector('.hamburguer_menu')
+ const button_close = document.querySelector('.close_menu')
 
  changes.addListener(validation);
  function validation(event){
      if(event.matches){
          button_hamburguer.addEventListener('click', showHidden);
+         button_close.addEventListener('click', showHidden);
      }
      else{
          button_hamburguer.removeEventListener('click', showHidden);
+         
      }
  }
  validation(changes)
 
  function showHidden() {
      if(menu.classList.contains('menu_active')){
-        menu.classList.remove('menu_active')   
+        menu.classList.remove('menu_active')
+        button_hamburguer.classList.remove('menu-hamburger_display') 
+        button_close.classList.remove('close_menu_active')
      }
      else{
          menu.classList.add('menu_active')
+         button_hamburguer.classList.add('menu-hamburger_display')
+         button_close.classList.add('close_menu_active')
+         
      }
  }
